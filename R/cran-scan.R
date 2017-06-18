c6d0734f2fba3f24ef41574b53645f18797816b3
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-06-18 22:32:03 +1000

    first commit :tada:

---
+++ R/cran-scan.R
@@ -0,0 +1,74 @@
+#' Search CRAN and return a tibble
+#'
+#' This uses tools::CRAN_package_db() then returns a tibble with the columns:
+#'     col_1....col_m. This function is currently internal as it is called in
+#'     scan_cran.
+#'
+#' @return a tibble
+#'
+#' @examples
+#'
+#' \dontrun{
+#' get_cran_tbl()
+#' }
+#'
+get_cran_tbl <- function(){
+
+  # big Thank You to Julia Silge for the code to do this in her blog post:
+  # https://juliasilge.com/blog/mining-cran-description/
+
+cran_db <- tools::CRAN_package_db()
+
+# the returned data frame has two columns with the same name???
+cran_db <- cran_db[,-65]
+
+# possibly make all the columns into lowercase
+
+# return it a tibble
+dplyr::as_tibble(cran_db)
+
+}
+
+#' Find packages mentioning key words in description.
+#'
+#' @param key_words character vector containing text to put into grepl
+#' @param when character vector either containing "last-day", "last-week", or
+#'     "last-month".
+#'
+#' @return a tibble with R package names matching some strings, and download
+#'     counts from the last month by default.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' scan_cran_tbl <- scan_cran("miss | imp*")
+#'
+#' }
+#'
+scan_cran <- function(key_words, when = "last-month"){
+
+cran_tbl <- get_cran_tbl()
+key_words <- "miss | imp"
+cran_scanned <- cran_tbl %>%
+  dplyr::select(Package,
+                Description) %>%
+  dplyr::mutate(has_missing = grepl(key_words, Description)) %>%
+  dplyr::filter(has_missing)
+
+# use cranlogs to find how often packages are downloaded
+cran_scanned_dl <- cranlogs::cran_downloads(packages = cran_scanned$Package,
+                                            when = when)
+
+cran_scanned_dl
+  dplyr::as_tibble() %>%
+  dplyr::group_by(package) %>%
+  dplyr::mutate(n_dl = sum(count)) %>%
+  dplyr::select(package, n_dl) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  dplyr::arrange(-n_dl)
+
+}
