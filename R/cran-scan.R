ba74b85f8a03da27f5256a34037fb1658ac41ed3
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-06-19 14:31:25 +1000

    limit cranlogs API call to 950 :pencil:

--- R/cran-scan.R
+++ R/cran-scan.R
@@ -51,24 +51,34 @@ dplyr::as_tibble(cran_db)
 scan_cran <- function(key_words, when = "last-month"){
 
 cran_tbl <- get_cran_tbl()
-key_words <- "miss | imp"
+# key_words = "miss | imp"
+# when = "last-month"
+
 cran_scanned <- cran_tbl %>%
   dplyr::select(Package,
                 Description) %>%
-  dplyr::mutate(has_missing = grepl(key_words, Description)) %>%
-  dplyr::filter(has_missing)
+  dplyr::mutate(has_key_words = grepl(key_words, Description)) %>%
+  dplyr::filter(has_key_words) %>%
+  # make lower case
+  dplyr::rename(package = Package,
+                description = Description)
 
 # use cranlogs to find how often packages are downloaded
-cran_scanned_dl <- cranlogs::cran_downloads(packages = cran_scanned$Package,
+# currently only takes the first 950
+# possibly due to API constraints?
+cran_scanned_dl <- cranlogs::cran_downloads(packages = cran_scanned$package[1:950],
                                             when = when)
 
-cran_scanned_dl
+cran_scanned_dl %>%
   dplyr::as_tibble() %>%
   dplyr::group_by(package) %>%
   dplyr::mutate(n_dl = sum(count)) %>%
   dplyr::select(package, n_dl) %>%
   dplyr::ungroup() %>%
   dplyr::distinct() %>%
-  dplyr::arrange(-n_dl)
+  dplyr::arrange(-n_dl) %>%
+  dplyr::left_join(cran_scanned,
+                   by = "package") %>%
+  dplyr::select(package, description, n_dl)
 
 }
