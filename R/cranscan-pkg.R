16642f18fecd8e0089f50d40de8beed443f30924
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-06-19 14:30:45 +1000

    add cranscan pkg helpfile :cherries:

---
+++ R/cranscan-pkg.R
@@ -0,0 +1,10 @@
+#' cranscan
+#'
+#' cranscan makes it fun to explore cran packages
+#'
+#' @name cranscan
+#' @docType package
+#' @importFrom magrittr %>%
+NULL
+
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
